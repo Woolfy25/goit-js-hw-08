e03d63ef156a48faafec63e854f7458105773e7c
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-05-29 21:05:45 +0300

    homework 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,8 @@
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import '../sass/_common.scss';
-// import '../css/01-gallery.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
 
 const createItemsMarkup = galleryItems
   .map(({ preview, original, description }) => {
