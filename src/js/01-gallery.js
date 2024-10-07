0a8c823651477a27d53f0796c5c072096a30ce0a
Ciutre Ramon <ramonciutre7@gmail.com>
2024-10-07 09:19:25 +0300

    addressing problems

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,25 @@
 // Add imports above this lines
-import { galleryItems } from '../js/gallery-items.js';
+import { galleryItems } from "../js/gallery-items.js";
 
 // Change code below this line
 
 console.log(galleryItems);
 
-const container = document.querySelector('.gallery');
+const container = document.querySelector(".gallery");
 
-galleryItems.forEach(image => {
-  let list = document.createElement('li');
-  list.setAttribute('class', 'gallery__item');
+galleryItems.forEach((image) => {
+  let list = document.createElement("li");
+  list.setAttribute("class", "gallery__item");
 
-  let link = document.createElement('a');
-  link.setAttribute('href', image.original);
-  link.setAttribute('class', 'gallery__link');
+  let link = document.createElement("a");
+  link.setAttribute("href", image.original);
+  link.setAttribute("class", "gallery__link");
 
-  let img = document.createElement('img');
-  img.setAttribute('src', image.preview);
+  let img = document.createElement("img");
+  img.setAttribute("src", image.preview);
   //   img.setAttribute("data-source", image.original);
-  img.setAttribute('alt', image.description);
-  img.setAttribute('class', 'gallery__image');
+  img.setAttribute("alt", image.description);
+  img.setAttribute("class", "gallery__image");
 
   link.appendChild(img);
 
@@ -28,10 +28,10 @@ galleryItems.forEach(image => {
   container.append(list);
 });
 
-const clickContainerBox = event => {
+const clickContainerBox = (event) => {
   event.preventDefault();
-  if (event.target.classList.contains('gallery')) return;
-  const lightbox = new SimpleLightbox('.gallery a');
+  if (event.target.classList.contains("gallery")) return;
+  const lightbox = new SimpleLightbox(".gallery a");
 };
 
-container.addEventListener('click', clickContainerBox);
+container.addEventListener("click", clickContainerBox);
